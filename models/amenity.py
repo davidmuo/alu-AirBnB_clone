6e36b129c825b04acb22c5c30472960fb5d84562
davidmuo <d.muotohfra@alustudent.com>
2026-01-31 02:01:05 +0200

    done

---
+++ models/amenity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""_summary_
+    This module creates new instances of the Amenity class -- This
+    is visible in the frontend of this project
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """_summary_
+        This class inherits from a super class named - BaseModel
+        name(str): empty string
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """_summary_
+            This constructor method inherits from the constructor
+            method in BaseModel
+        """
+        super().__init__(*args, **kwargs)
---
+++ models/user.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""_summary_
+    This module creates new instances of the User class -- This is
+    visible in the frontend of this project
+"""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """_summary_
+        This class inherits from a super class named - BaseModel
+        email(str): The user's email
+        password(str): The user's password
+        first_name(str): The user's first_name
+        last_name(str): The user's last_name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """_summary_
+            This constructor method inherits from the constructor
+            method in BaseModel
+        """
+        super().__init__(*args, **kwargs)
